35d856b84426639f3b50d5922c4e8306ea18b426
jianchengwang <jiancheng_wang@yahoo.com>
2022-03-15 08:39:52 +0800

    todolist 1

--- todolist/lib/app/modules/home/controller.dart
+++ todolist/lib/app/modules/home/controller.dart
@@ -12,7 +12,9 @@ class HomeController extends GetxController {
   final chipIndex = 0.obs;
   final deleting = false.obs;
   final tasks = <Task>[].obs;
-
+  final task = Rx<Task?>(null);
+  final doingTodos = <dynamic>[].obs;
+  final doneTodos = <dynamic>[].obs;
 
   @override
   void onInit() {
@@ -35,11 +37,51 @@ class HomeController extends GetxController {
     deleting.value = value;
   }
 
+  void changeTask(Task? select) {
+    task.value = select;
+  }
+
+  void changeTodos(List<dynamic> select) {
+    doneTodos.clear();
+    doingTodos.clear();
+    for(int i=0; i<select.length; i++) {
+      var todo = select[i];
+      var status = todo['done'];
+      if(status == true) {
+        doneTodos.add(todo);
+      } else {
+        doingTodos.add(todo);
+      }
+    }
+  }
+
   bool addTask(Task task) {
     if(tasks.contains(task)) {
       return false;
     }
     tasks.add(task);
     return true;
+  }
+
+  void deleteTask(Task task) {
+    tasks.remove(task);
+  }
+
+  bool updateTask(Task task, String title) {
+    var todos = task.todos ?? [];
+    if(containTodo(todos, title)) {
+      return false;
+    }
+    var todo = {'title': title, 'done': false};
+    todos.add(todo);
+    var newTask = task.copyWith(todos: todos);
+    int oldIdx = tasks.indexOf(task);
+    tasks[oldIdx] = newTask;
+    tasks.refresh();
+    return true;
+  }
+
+  bool containTodo(List todos, String title) {
+    return todos.any((element) => element['title'] == title);
   }
 }
--- todolist/lib/app/modules/home/view.dart
+++ todolist/lib/app/modules/home/view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:todolist/app/data/models/task.dart';
 import 'package:todolist/app/modules/home/controller.dart';
 import 'package:todolist/app/core/utils/extensions.dart';
 import 'package:todolist/app/modules/home/widgets/add_card.dart';
+import 'package:todolist/app/modules/home/widgets/add_dialog.dart';
 import 'package:todolist/app/modules/home/widgets/task_card.dart';
 
 import '../../core/values/colors.dart';
@@ -33,6 +36,7 @@ class HomePage extends GetView<HomeController> {
                   children: [
                     ...controller.tasks.map((element) =>
                       LongPressDraggable(
+                        data: element,
                         onDragStarted: () => controller.changeDeleting(true),
                         onDraggableCanceled: (_,__) => controller.changeDeleting(false),
                         onDragEnd: (_) => controller.changeDeleting(false),
@@ -50,12 +54,26 @@ class HomePage extends GetView<HomeController> {
           ],
         ),
       ),
-      floatingActionButton: Obx(
-        () => FloatingActionButton(
-          backgroundColor: controller.deleting.value ? Colors.red: blue,
-          onPressed: () {},
-          child: Icon(controller.deleting.value ? Icons.delete : Icons.add),
-        ),
+      floatingActionButton: DragTarget<Task>(
+        builder: (_,__,___) {
+          return Obx(
+                () => FloatingActionButton(
+              backgroundColor: controller.deleting.value ? Colors.red: blue,
+              onPressed: () {
+                if(controller.tasks.isNotEmpty) {
+                  Get.to(() => AddDialog(), transition: Transition.downToUp);
+                } else {
+                  EasyLoading.showInfo('Please create your task type');
+                }
+              },
+              child: Icon(controller.deleting.value ? Icons.delete : Icons.add),
+            ),
+          );
+        },
+        onAccept: (Task task) {
+          controller.deleteTask(task);
+          EasyLoading.showSuccess('Delete Sucess');
+        },
       ),
     );
   }
